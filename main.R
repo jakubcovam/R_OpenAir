fa585ffd3d165b2d6f1e4a5e0dd3e8768c9d1728
Michala Jakubcova <michala.jakubcova@gmail.com>
2023-01-31 13:36:31 +0100

    Update main.R

--- main.R
+++ main.R
@@ -1,1 +1,21 @@
+# Import libraries
+library(openair)
+library(tidyverse)
+library(worldmet)
 
+# Access air quality data
+myData = importAURN(
+  site = "my1",
+  year = 2000:2022,
+  data_type = "hourly",
+  pollutant = "all",
+  meta = TRUE,
+  ratified = FALSE,
+  to_narrow = TRUE,
+  verbose = FALSE,
+  progress = TRUE
+)
+
+head(myData)
+
+# Access meteorological data
